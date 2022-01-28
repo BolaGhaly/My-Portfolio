d34f691300b9062daa289423df71927d7874142f
Bola Ghaly <bola.ghaly@cix.csi.cuny.edu>
2022-01-28 01:06:21 -0500

    Updated skills and tools section

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,13 +1,27 @@
 import React from "react";
+
+/* Logos */
 import HTML_Logo from "../assets/skills/HTML-Logo.svg";
 import CSS_Logo from "../assets/skills/CSS-logo.svg";
 import Javascript_Logo from "../assets/skills/Javascript-Logo.svg";
 import ReactJS_Logo from "../assets/skills/ReactJS-Logo.svg";
+import NodeJS_Logo from "../assets/skills/NodeJS-Logo.svg";
+import ExpressJS_Logo from "../assets/skills/ExpressJS.svg";
+import PostgreSQL_Logo from "../assets/skills/PostgreSQL-Logo.svg";
+// import REST_API_Logo from "../assets/skills/REST-API-Logo.png";
+// import REST_API_Logo from "../assets/skills/REST-API-Logo.svg";
+import Postman_Logo from "../assets/skills/Postman-Logo.svg";
+import CPP_Logo from "../assets/skills/Cplusplus-Logo.svg";
+import Python_Logo from "../assets/skills/Python-Logo.svg";
+import Git_Logo from "../assets/skills/Git-logo.svg";
+
+
+
 
 const Skills = () => {
   return (
     <section className="skills-section" id="skills">
-      <h2 className="text-center">Skills</h2>
+      <h2 className="text-center">Skills &amp; Tools</h2>
       <div className="skills-container-div">
         <ul className="skills-container-ul">
           <li>
@@ -35,25 +49,52 @@ const Skills = () => {
             </div>
           </li>
           <li>
-            <div className="skills-container-item"></div>
+            <div className="skills-container-item">
+              <img src={NodeJS_Logo} alt="Node.js Logo" />
+              <p>Node.js</p>
+            </div>
           </li>
           <li>
-            <div className="skills-container-item"></div>
+            <div className="skills-container-item">
+              <img src={ExpressJS_Logo} alt="Express.js Logo" />
+              <p>Express.js</p>
+            </div>
           </li>
           <li>
-            <div className="skills-container-item"></div>
+            <div className="skills-container-item">
+              <img src={PostgreSQL_Logo} alt="PostgreSQL Logo" />
+              <p>PostgreSQL</p>
+            </div>
           </li>
           <li>
-            <div className="skills-container-item"></div>
+            <div className="skills-container-item">
+              {/* <img src={REST_API_Logo} alt="REST API Logo" />
+              <p>REST API</p> */}
+            </div>
           </li>
           <li>
-            <div className="skills-container-item"></div>
+            <div className="skills-container-item">
+              <img src={Postman_Logo} alt="Postman Logo" />
+              <p>Postman</p>
+            </div>
           </li>
           <li>
-            <div className="skills-container-item"></div>
+            <div className="skills-container-item">
+              <img src={CPP_Logo} alt="C++ Logo" />
+              <p>C++</p>
+            </div>
           </li>
           <li>
-            <div className="skills-container-item"></div>
+            <div className="skills-container-item">
+              <img src={Python_Logo} alt="Python Logo" />
+              <p>Python</p>
+            </div>
+          </li>
+          <li>
+            <div className="skills-container-item">
+              <img src={Git_Logo} alt="Git Logo" />
+              <p>Git</p>
+            </div>
           </li>
         </ul>
       </div>
