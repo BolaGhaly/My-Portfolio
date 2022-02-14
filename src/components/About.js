2edac55c7fafe8c22ee98d1356604dc6e978215a
Bola Ghaly <polaghaly63@gmail.com>
2022-02-13 22:01:22 -0500

    Update "About Me" section

--- src/components/About.js
+++ src/components/About.js
@@ -5,11 +5,15 @@ const About = () => {
     <section className="about-section" id="about">
       <h2>About Me</h2>
       <p>
-        I am currently an undergraduate student at CUNY College of Staten Island
-        majoring in Computer Science. I am intellectually curious and passionate
-        about problem-solving and learning new technologies and tech stacks to expand my technical
-        skill set. I am currently seeking internships for
-        Summer of 2022.
+        An undergraduate student at CUNY College of Staten Island majoring in
+        Computer Science (bachelor's degree). I am intellectually curious and
+        passionate about problem-solving and learning new technologies and tech
+        stacks to expand my technical skill set. I am currently seeking an
+        internship for Summer of 2022.
+        <br />
+        <br />
+        Also, I like to listen to music, play sports and video games, and
+        workout too!! &#128512;
       </p>
     </section>
   );
