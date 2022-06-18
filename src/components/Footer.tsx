f4fc25461d8cfb8632ef6893d8d22ed883208753
Bola Ghaly <polaghaly63@gmail.com>
2022-06-18 02:01:48 -0400

    Update Footer.tsx to improve accessibility score

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,24 +1,27 @@
-/* eslint-disable jsx-a11y/anchor-has-content */
 import React from "react";
 
 const Footer = () => {
-    return (
-      <footer className="footer text-center">
-        <a
-          href="https://linkedin.com/in/bolaghaly"
-          target="_blank"
-          rel="noreferrer"
-          className="fab fa-linkedin"
-        ></a>
-        <a
-          href="https://github.com/BolaGhaly"
-          target="_blank"
-          rel="noreferrer"
-          className="fab fa-github"
-        ></a>
-        <p>Copyright &copy; 2022 by Bola Ghaly. All rights reserved.</p>
-      </footer>
-    );
-}
+  return (
+    <footer className="footer text-center">
+      <a
+        href="https://linkedin.com/in/bolaghaly"
+        target="_blank"
+        rel="noreferrer"
+        className="fab fa-linkedin"
+      >
+        <span>Bola Ghaly's Linkedin Profile</span>
+      </a>
+      <a
+        href="https://github.com/BolaGhaly"
+        target="_blank"
+        rel="noreferrer"
+        className="fab fa-github"
+      >
+        <span>Bola Ghaly's GitHub Profile</span>
+      </a>
+      <p>Copyright &copy; 2022 by Bola Ghaly. All rights reserved.</p>
+    </footer>
+  );
+};
 
 export default Footer;
